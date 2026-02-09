27ddcd76931a4339b0c17c2164d87d697fed4ec7
polin-sv <polinasvetlay@icloud.com>
2026-02-09 17:09:55 +0300

    Add project files.

---
+++ Task01Project/logic.cpp
@@ -0,0 +1,19 @@
+﻿#include "logic.h"
+
+string count_likes(int day, int like) {
+
+	if (day <= 0 || like < 0) {
+		return "Error.";
+	}
+
+	string result;
+
+	for (int i = 1; i <= day; i++) {
+		int likes_per_day = i * like;
+		result += "Day " + to_string(i) + ": "
+			+ to_string(likes_per_day) + " likes\n";
+
+	}
+
+	return result;
+}
