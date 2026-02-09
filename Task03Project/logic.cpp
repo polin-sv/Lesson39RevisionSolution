eab846e7ac7ca74a40c4a14f0537e3953f940192
polin-sv <polinasvetlay@icloud.com>
2026-02-09 17:49:10 +0300

    soft task03

--- Task03Project/logic.cpp
+++ Task03Project/logic.cpp
@@ -1,5 +1,18 @@
 ﻿#include "logic.h"
 
 bool is_power_of_two(int number){	
-	return false;
+	if (number <= 0) {
+		return false;
+	}
+
+	while (number > 1) {
+		if (number % 2 != 0) {
+			return false;
+		}
+		number /= 2;
+
+	}
+
+	return true;
+	
 }
