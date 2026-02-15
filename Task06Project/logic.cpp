ea03ed2d7782612ad92640dafdafa81196698bf2
polin-sv <polinasvetlay@icloud.com>
2026-02-15 14:30:53 +0300

    soft06

--- Task06Project/logic.cpp
+++ Task06Project/logic.cpp
@@ -1,5 +1,26 @@
 ﻿#include "logic.h"
 
 long long get_fibo_number(int index) {
-	return 0LL;
+	long long first = 0;
+	long long second = 1;
+	long long third = 0;
+	if (index <= 0) {
+		return -1;
+	}
+
+	if (index == 1) {
+		return 0;
+	}
+	if (index == 2) {
+		return 1;
+	}
+	else {
+
+		for (int i = 3; i <= index; i++) {
+			third = first + second;
+			first = second;
+			second = third;
+		}
+	}
+	return third;
 }
